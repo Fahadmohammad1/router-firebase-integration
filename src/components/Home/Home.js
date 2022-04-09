f0e9509ee8cff44267ecb1b60dfddc014d1fb779
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-04-10 01:49:08 +0600

    complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,13 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
-
+import { useAuthState } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+const auth = getAuth(app);
 const Home = () => {
+  const [user] = useAuthState(auth);
   return (
     <div>
-      <h1>hi this is home</h1>
+      <h1>Your Name is: {user ? user.displayName : "no one here"} </h1>
     </div>
   );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,33 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
-import useFirebase from "../../Hooks/useFirebase";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
+import app from "../../firebase.init";
 
+const auth = getAuth(app);
 const Login = () => {
-  const { handleSignIn } = useFirebase();
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
+
+  const handleGoogleSignIn = () => {
+    signInWithGoogle().then(() => {
+      navigate(from, { replace: true });
+    });
+  };
+
   return (
     <div>
       <h3>Please Login</h3>
-      <button onClick={handleSignIn} style={{ margin: "20px" }}>
+      <button
+        onClick={() => {
+          handleGoogleSignIn();
+        }}
+        style={{ margin: "20px" }}
+      >
         Google
       </button>
       <br />
